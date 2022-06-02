38da977919c65574b6f63c525cc387d02219bb0e
skogard <skogard@protonmail.com>
2022-06-01 22:14:47 -0400

    a feature to restrict chainId

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class C0 {
   async init(o) {
     this.web3 = o.web3
     this.key = o.key
+    this.chainId = o.chain
 
     if (this.key) {
       this.wallet = this.web3.eth.accounts.privateKeyToAccount("0x" + this.key)
@@ -38,10 +39,22 @@ class C0 {
     o.account = this.account
     o.wallet = this.wallet
 
+    if (this.chainId) {
+      let chainId = await this.web3.eth.getChainId()
+      // if the chainId is specified, it must match the currently connected chain
+      if (parseInt(chainId) === parseInt(this.chainId)) {
+        // correct. do nothing
+      } else {
+        throw new Error(`[Error] Wrong network. Expected chainId ${this.chainId}, but connected to ${chainId}`); 
+      }
+    } else {
+      // if the chainId is not specified, pass.
+    }
     this.collection.init(o)
     this.token.init(o)
     this.util.init(o)
     this.gift.init(o)
+
   }
 }
 module.exports = C0
